2ee17a2b20e6044df031dca8df53db64b92af874
Rahman <museabdirahman@gmail.com>
2025-05-20 17:26:16 +0100

    initialised project with project structure, route groups etc

---
+++ src/app/(marketing)/page.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Image
+        src="/images/next.svg"
+        alt="Next.js Logo"
+        width={180}
+        height={37}
+        priority
+      />
+      <p className="text-lg">This is a sample Next.js application.</p>
+      <p className="text-lg">It uses Tailwind CSS for styling.</p>
+      <p className="text-lg">Enjoy coding!</p>
+    </div>
+  );
+}
