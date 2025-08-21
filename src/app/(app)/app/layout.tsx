602a1065de74973f33fd16795f86bf2f96aa3e7e
Rahman <museabdirahman@gmail.com>
2025-08-21 14:00:18 +0100

    feat:Added prisma orm and temp local sqlite db which is seeded with initial data

--- src/app/(app)/app/layout.tsx
+++ src/app/(app)/app/layout.tsx
@@ -3,15 +3,11 @@ import AppHeader from "@/components/AppHeader";
 import BackgroundPattern from "@/components/BackgroundPattern";
 import PetContextProvider from "@/context/PetContextProvider";
 import SearchContextProvider from "@/context/SearchContextProvider";
-import { Pet } from "@/lib/types";
+import prisma from "@/lib/db";
 import React from "react";
 
 const layout = async ({ children }: { children: React.ReactNode }) => {
-  const response = await fetch(
-    "https:bytegrad.com/course-assets/projects/petsoft/api/pets"
-  );
-  if (!response.ok) throw new Error();
-  const data: Pet[] = await response.json();
+  const data = await prisma.pet.findMany()
 
   return (
     <>
