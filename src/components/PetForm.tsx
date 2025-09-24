7a4272b34953f899ae63447fd41af25e46e239cc
Rahman <museabdirahman@gmail.com>
2025-09-24 21:33:45 +0100

    added react-hook-form for petform validation

--- src/components/PetForm.tsx
+++ src/components/PetForm.tsx
@@ -7,20 +7,40 @@ import { Textarea } from "./ui/textarea";
 import { usePetContext } from "@/lib/hooks";
 import { imagePlaceholder, sleep } from "@/lib/utils";
 import PetFormBtn from "./PetFormBtn";
+import { useForm } from "react-hook-form";
 
 type PetFormProps = {
   actionType: "add" | "edit";
   onFormSubmit: () => void; // Optional callback for form submission
 };
 
+type TPetForm = {
+  name: string;
+  ownerName: string;
+  imageUrl: string;
+  age: number;
+  notes?: string;
+};
+
 function PetForm({ actionType, onFormSubmit }: PetFormProps) {
   // Use the custom hook to access pet context
   const { handleAddPet, selectedPet, handleEditPet } = usePetContext();
 
+  const {
+    register,
+    formState: { errors },
+    trigger,
+  } = useForm<TPetForm>();
+
   return (
     <form
       action={async (formData) => {
+        // Validate the form before submission
+        const results = await trigger();
+        if (!results) return;
+        // Call the optional onFormSubmit callback
         onFormSubmit();
+
         const petData = {
           name: formData.get("name") as string,
           ownerName: formData.get("ownerName") as string,
@@ -42,50 +62,58 @@ function PetForm({ actionType, onFormSubmit }: PetFormProps) {
           <Label htmlFor="name">Name</Label>
           <Input
             id="name"
-            name="name"
-            type="text"
-            required
-            defaultValue={actionType == "edit" ? selectedPet?.name : ""}
+            {...register("name", {
+              required: "Name is required",
+              minLength: {
+                value: 3,
+                message: "Name must be at least 3 characters",
+              },
+            })}
           />
+          {errors.name && (
+            <p className="text-sm text-red-600">
+              {errors.name.message as string}
+            </p>
+          )}
         </div>
         <div className="space-y-1">
           <Label htmlFor="ownerName">Owner Name</Label>
           <Input
             id="ownerName"
-            name="ownerName"
-            type="text"
-            required
-            defaultValue={actionType == "edit" ? selectedPet?.ownerName : ""}
+            {...register("ownerName", { required: "Owner name is required",maxLength: { value: 10, message: "Owner name must be at most 10 characters" } })}
           />
+          {errors.ownerName && (
+            <p className="text-sm text-red-600">
+              {errors.ownerName.message as string}
+            </p>
+          )}
         </div>
         <div className="space-y-1">
           <Label htmlFor="imageUrl">Image url</Label>
-          <Input
-            id="imageUrl"
-            name="imageUrl"
-            type="text"
-            defaultValue={actionType == "edit" ? selectedPet?.imageUrl : ""}
-          />
+          <Input id="imageUrl" {...register("imageUrl")} />
+          {errors.imageUrl && (
+            <p className="text-sm text-red-600">
+              {errors.imageUrl.message as string}
+            </p>
+          )}
         </div>
         <div className="space-y-1">
           <Label htmlFor="age">Age</Label>
-          <Input
-            id="age"
-            name="age"
-            type="number"
-            required
-            defaultValue={actionType == "edit" ? selectedPet?.age : ""}
-          />
+          <Input id="age" {...register("age")} />
+          {errors.age && (
+            <p className="text-sm text-red-600">
+              {errors.age.message as string}
+            </p>
+          )}
         </div>
         <div className="space-y-1">
           <Label htmlFor="notes">Notes</Label>
-          <Textarea
-            id="notes "
-            name="notes"
-            rows={3}
-            required
-            defaultValue={actionType == "edit" ? selectedPet?.notes : ""}
-          />
+          <Textarea id="notes " {...register("notes")} />
+          {errors.notes && (
+            <p className="text-sm text-red-600">
+              {errors.notes.message as string}
+            </p>
+          )}
         </div>
       </div>
       <PetFormBtn actionType={actionType} />
